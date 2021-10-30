55db5e215ef17d0a1b44d1f075bdb22d25080e04
shopzoid <shopzoid.id@gmail.com>
2021-10-30 11:32:09 +0700

    first commit

---
+++ firebase/data.js
@@ -0,0 +1,60 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+  apiKey: "AIzaSyDTBMoiWOrQnU8PImOAbtVSR8FcF8ChpfI",
+  authDomain: "shopzoid-daftar.firebaseapp.com",
+  databaseURL: "https://shopzoid-daftar-default-rtdb.firebaseio.com",
+  projectId: "shopzoid-daftar",
+  storageBucket: "shopzoid-daftar.appspot.com",
+  messagingSenderId: "881147393125",
+  appId: "1:881147393125:web:5e03ec46dd578c8a00c28c",
+  measurementId: "G-H19Q4CSSGL"
+  };
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('Contact-Us');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e) {
+    e.preventDefault();
+    setTimeout(function(){window.location.href = "terkirim.html" ;}, 3000);
+  
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var instagram = getInputVal('instagram');
+  
+    // Save message
+    saveMessage(name, email, phone, instagram);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function() {
+      document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id) {
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, phone, instagram) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email: email,
+      phone: phone,
+      instagram: instagram,
+    });
+  }
