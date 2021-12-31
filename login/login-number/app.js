f236198536246dc56cd12092d4b435dca97c4d66
joxiplayer <joxiplayer.id@gmail.com>
2021-12-31 11:10:04 +0700

    first commit

--- login/login-number/app.js
+++ login/login-number/app.js
@@ -2,14 +2,14 @@
 
 var firebaseConfig = {
 
-    apiKey: "AIzaSyA6in5d89RIbzA0DzHvjxREC4fBPh_8Ngw",
-  authDomain: "shopzoid-id.firebaseapp.com",
-  databaseURL: "https://shopzoid-id-default-rtdb.firebaseio.com",
-  projectId: "shopzoid-id",
-  storageBucket: "shopzoid-id.appspot.com",
-  messagingSenderId: "839294207212",
-  appId: "1:839294207212:web:d59868050d01bc8e6791c9",
-  measurementId: "G-M8FEBWLE57",
+    apiKey: "AIzaSyCORThhpBzHN6_4Z1U-4kI__A6xf5fRuzA",
+    authDomain: "shopzoid-app.firebaseapp.com",
+    databaseURL: "https://shopzoid-app-default-rtdb.firebaseio.com",
+    projectId: "shopzoid-app",
+    storageBucket: "shopzoid-app.appspot.com",
+    messagingSenderId: "124724344551",
+    appId: "1:124724344551:web:7be967100ee7a075469442",
+    measurementId: "G-XFNSKZGLNM",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -22,19 +22,19 @@ var message_value = document.querySelector('.message');
 
 
 // check if user is logged in or not
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        if(window.location.pathname != '/../daftar/index.html'){
+        if (window.location.pathname != '/../daftar/index.html') {
             window.location = '../index.html';
         }
     } else {
-        if(window.location.pathname === '/../daftar/index.html'){
+        if (window.location.pathname === '/../daftar/index.html') {
             window.location = 'index.html';
         }
     }
 });
 
-if(window.location.pathname != '/../index.html'){
+if (window.location.pathname != '/../index.html') {
     // verification captcha setting 
     window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
         'size': 'invisible',
@@ -44,42 +44,42 @@ if(window.location.pathname != '/../index.html'){
     });
     const appVerifier = window.recaptchaVerifier;
     firebase.auth().settings.appVerificationDisabledForTesting = true; // turn this off in production stage
-    
+
     // show error message function 
 
-function showErrorMessage(erro_message){
+    function showErrorMessage(erro_message) {
 
-    message.style.display = 'block';
-    message_value.innerText = erro_message;
-    console.log(erro_message);
-    setTimeout(function(){
-        message.style.display = 'none';
-    }, 3000);
-}
+        message.style.display = 'block';
+        message_value.innerText = erro_message;
+        console.log(erro_message);
+        setTimeout(function () {
+            message.style.display = 'none';
+        }, 3000);
+    }
 
     // user login
-    if(form){
-        form.addEventListener('submit', function(e) {
+    if (form) {
+        form.addEventListener('submit', function (e) {
             e.preventDefault();
             let phone_number = form.phone.value;
-        
+
             // setup invisible recaptcha 
             firebase.auth().signInWithPhoneNumber(phone_number, appVerifier)
-            .then((confirmationResult) => {
-                console.log("OTP SEND", confirmationResult);
-                form.style.display = 'none';
-                otp_form.style.display = 'block';
-                window.confirmationResult = confirmationResult;
-            }).catch((error) => {
-                showErrorMessage(error.message);
-            });
-            
+                .then((confirmationResult) => {
+                    console.log("OTP SEND", confirmationResult);
+                    form.style.display = 'none';
+                    otp_form.style.display = 'block';
+                    window.confirmationResult = confirmationResult;
+                }).catch((error) => {
+                    showErrorMessage(error.message);
+                });
+
         })
     }
 
     // verify otp 
-    if(otp_form){
-        otp_form.addEventListener('submit', function(e) {
+    if (otp_form) {
+        otp_form.addEventListener('submit', function (e) {
             e.preventDefault();
             let otp_number = otp_form.otp_value.value;
             confirmationResult.confirm(otp_number).then((result) => {
@@ -97,12 +97,12 @@ function showErrorMessage(erro_message){
 
 
 // sign out  
-if(sign_out){
-    sign_out.addEventListener('click', function(e) {
+if (sign_out) {
+    sign_out.addEventListener('click', function (e) {
         firebase.auth().signOut().then(() => {
             window.location = 'index.html';
         }).catch((error) => {
-        // An error happened.
+            // An error happened.
         });
     })
 }
